7093c5a36a9f4cb43f8676c556dc93eba24fb3d3
רועי נזרי <roeinizri1@gmail.com>
2024-08-21 15:05:26 +0300

    Updates

--- scripts.js
+++
@@ -1,8 +0,0 @@
-const timeout = setTimeout(function() {
-    window.location.reload();
-  }, 5000);
-  
-  // Clear the timeout if the page loads successfully
-  window.onload = function() {
-    clearTimeout(timeout);
-  };
